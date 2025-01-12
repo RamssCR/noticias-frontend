ed5919bab036b1e3d0ce28dc980eddf11b4855f9
Ramses Perez <ramsesgpm@gmail.com>
2025-01-12 17:25:11 -0500

    typography added to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,13 @@ export default {
         'body-main': '#f0f0f0',
         main: '#3C9E00',
         'main-complementary': '#426B1F'
-      }
+      },
+      gridTemplateColumns: {
+        '3/4': '51% 49%'
+      },
+      fontFamily: {
+        highlight: ['"Inknut Antiqua"', 'serif']
+      },
     },
   },
   plugins: [],
